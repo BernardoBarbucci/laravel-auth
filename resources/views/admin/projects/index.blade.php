6707ad3029ac88a81b30dfa87ddbe770cfcb7cf0
BernardoBarbucci <bernardobarbucci@gmail.com>
2024-02-15 16:05:20 +0100

    foreach per index -> all projects completato

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -5,5 +5,29 @@
         <section class="col-10 offset-1 bg-dark">
             <h1 class="text-warning text-center">im the index</h1>
         </section>
+        <section class="col-12 bg-dark">
+            <div class="card" style="width: 15rem;">
+                <div class="row">
+                    @foreach ($projects as $project)
+                        <div class="col-md-4 mb-4">
+                            <div class="card" style="width: 18rem;">
+                                <img src="{{ asset('images/' . $project['image']) }}" class="card-img-top" alt="{{ $project['title'] }}">
+                                <div class="card-body">
+                                    <h5 class="card-title">{{ $project['title'] }}</h5>
+                                    <p class="card-text">{{ $project['description'] }}</p>
+                                </div>
+                                <ul class="list-group list-group-flush">
+                                    <li class="list-group-item">Length: {{ $project['length'] }}</li>
+                                    <li class="list-group-item">Duration: {{ $project['duration'] }}</li>
+                                </ul>
+                                <div class="card-body">
+                                    <a href="#" class="card-link">Card link</a>
+                                    <a href="#" class="card-link">Another link</a>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+        </section>
     </main>
 @endsection

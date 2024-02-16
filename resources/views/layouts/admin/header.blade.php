b53ace6270b1ee88f2b512ac9961b613be6b43f1
BernardoBarbucci <bernardobarbucci@gmail.com>
2024-02-16 16:19:09 +0100

    create file completato con button nell'header

--- resources/views/layouts/admin/header.blade.php
+++ resources/views/layouts/admin/header.blade.php
@@ -2,4 +2,11 @@
     <section class="text-center py-3">
         <h1 class="fw-bold text-success">Admin Projects</h1>
     </section>
+    <section>
+        <div class="d-flex flex-row justify-content-end">
+            <a href="{{ route('admin.data.create') }}" class="btn btn-success me-3">Create</a>
+            {{-- <a href="{{ route('admin.data.edit', $project) }}" class="btn btn-warning me-3">Edit</a> --}}
+            {{-- <a href="{{ route('admin.data.show', $project) }}" class="btn btn-primary me-3">Show</a> --}}
+        </div>
+    </section>
 </header>
